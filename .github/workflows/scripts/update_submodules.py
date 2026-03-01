1f7d3ceffd380633ba67953dfc0fb2795ad2c607
leowajda <dev.leowajda@tuta.io>
2026-03-01 19:57:09 +0100

    fix(github-actions): report only updated submodules in commit message

--- .github/workflows/scripts/update_submodules.py
+++ .github/workflows/scripts/update_submodules.py
@@ -20,10 +20,8 @@ for metadata in completed_process.stdout.splitlines():
     url = f"{SERVER_URL}/{ACTOR}/{submodule}/commit/{commit_sha}"
     details.append(url)
 
-if not details:
-    raise SystemExit(0)
-
-with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
-    noun = "submodule" if len(details) == 1 else "submodules"
-    commit_msg = f'ci(docs): update lang {noun} to latest version\n' + "\n".join(details)
-    print(f'commit_msg<<EOF\n{commit_msg}\nEOF', file=f)
+if details:
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
+        noun = "submodule" if len(details) == 1 else "submodules"
+        commit_msg = f'ci(docs): update lang {noun} to latest version\n' + "\n".join(details)
+        print(f'commit_msg<<EOF\n{commit_msg}\nEOF', file=f)
