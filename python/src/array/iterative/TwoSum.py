b320326882700d0b5f1c568eb019d0e1ea8900a0
leowajda <dev.leowajda@tuta.io>
2026-04-18 15:54:48 +0200

    solution(leetcode): update iterative 'Two Sum'

--- python/src/array/iterative/TwoSum.py
+++ python/src/array/iterative/TwoSum.py
@@ -1,11 +1,13 @@
 class TwoSum:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        first_index_by_value: dict[int, int] = {}
+        index_by_value: dict[int, int] = {}
 
         for index, value in enumerate(nums):
-            if (match_index := first_index_by_value.get(target - value)) is not None:
+            complement = target - value
+            match_index = index_by_value.get(complement)
+            if match_index is not None:
                 return [match_index, index]
 
-            first_index_by_value.setdefault(value, index)
+            index_by_value[value] = index
 
         return [-1, -1]
